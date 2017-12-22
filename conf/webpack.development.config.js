88f9062c7afb12930aa38265144828b3ea23325d
dmitry.azarov <tetemp>
2017-12-22 10:37:52 +0300

    modify dev config

--- conf/webpack.development.config.js
+++ conf/webpack.development.config.js
@@ -38,26 +38,6 @@ export default new Config().extend('conf/webpack.base.config.js').merge({
                   ],
                   fallback: "style-loader"
                 })
-        // {
-        //   loader: 'style-loader', // inject CSS to page
-        // },
-        // {
-        //   loader: 'css-loader', // translates CSS into CommonJS modules
-        //   options: {
-        //     modules: false,
-        //     importLoaders: 2,
-        //     localIdentName: "[local]__[hash:base64:5]",
-        //     // minimize: false
-        //   }
-        // },
-        // {
-        //   loader: 'postcss-loader',
-        //   options: { plugins: [precss(), autoprefixer()]}
-        // },
-        // {
-        //   loader: 'sass-loader', // compiles SASS to CSS
-        // }
-      //]
       },
       {
       test: /\.(gif|png|jpe?g|svg)$/i,
