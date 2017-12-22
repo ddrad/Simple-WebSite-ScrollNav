e79dad55d1a68bccec63261e23b3148d7ae0e3c8
dmitry.azarov <tetemp>
2017-12-22 10:27:15 +0300

    fixed updating value of list if address was changed

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import './scss/main.scss';
 import $ from "jquery";
 
 $(document).ready(function () {
- 
   addToTagList();
 
+  if ("onhashchange" in window) {
+    console.log("The browser supports the hashchange event!");
+  }
+  else{
+      console.log("The browseris not supports the hashchange event!");
+    keepTrackOfHashChanges();
+  }
+
+  window.onhashchange = locationHashChanged;
   $('.add_tag input').keyup(function () {
     let empty = false;
     $('.add_tag input').each(function () {
@@ -12,7 +20,6 @@ $(document).ready(function () {
         empty = true;
       }
     });
-
     if (empty) {
       $('.add_tag button').attr('disabled', 'true');
     } else {
@@ -39,22 +46,46 @@ $(document).ready(function () {
     let vars = query.split("=");
     for (let key in vars) {
       if (key == 1) {
-        var pair = vars[key].split(",");
+        let pair = vars[key].split(",");
         pair.pop();
         window.location.replace('#tags='.concat(pair));
         addToTagList();
       }
-    } 
+    }
   });
 });
+function locationHashChanged() {
+        addToTagList();
+}
+// this is function if browser is not supported onhashchange event
+function keepTrackOfHashChanges () {
+  let location = window.location,
+    oldURL = location.href,
+    oldHash = location.hash;
 
+  setInterval(function() {
+   let newURL = location.href,
+     newHash = location.hash;
+
+   if (newHash != oldHash && typeof window.onhashchange === "function") {
+     window.onhashchange({
+       type: "hashchange",
+       oldURL: oldURL,
+       newURL: newURL
+     });
+
+     oldURL = newURL;
+     oldHash = newHash;
+   }
+ }, 100);
+}
 function addToTagList() {
   $('ul.app-list').html('');
   let query = window.location.hash.substring(1);
   let vars = query.split("=");
   for (let key in vars) {
     if (key == 1) {
-      var pair = vars[key].split(",");
+      let pair = vars[key].split(",");
       for (let val in pair) {
         $('ul.app-list').append("<li>" + pair[val] + "</li>").trigger("create");
       }
