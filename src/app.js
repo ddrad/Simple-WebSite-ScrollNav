ac4d3f4c0dd8dc869b2abbd7c8b953074a1868e9
dmitry.azarov <tetemp>
2017-12-29 15:49:45 +0300

    fix scroll

--- src/app.js
+++ src/app.js
@@ -8,58 +8,79 @@ $(document).ready(function () {
   let headerHeight = elementHeader.outerHeight();
   let contentHeaderHeight = elementContentHeader.innerHeight();
 
+  if ("onhashchange" in window) {
+    console.log("The browser supports the hashchange event!");
+  }
+  else {
+    console.log("The browseris not supports the hashchange event!");
+    //keepTrackOfHashChanges();
+  }
+
+  $(window).on('hashchange', function () {
+    let target = location.hash;
+    if (target) {
+       return goToSection(target, event);
+    }
+    //  console.log(target);
+    //   console.log($('ul#menu li a'));
+    //  console.log($('ul#menu li a[href="'+target+'"]'));//
+   // $('ul#menu li a[href="'+target+'"]').trigger('click');
+  });
+
   $(window).on('resize', function () {
     headerOffset = elementHeader.offset();
     headerHeight = elementHeader.outerHeight();
     contentHeaderHeight = elementContentHeader.innerHeight();
     // let contentHeaderWidth = elementHeader.css('width');
-    addEventListener('scroll', function() {return onScroll(contentHeaderHeight);})
+    addEventListener('scroll', function () { return onScroll(contentHeaderHeight); })
   }).resize();
 
-  $(window).on('scroll', function() {return onScroll(contentHeaderHeight);});
+  $(window).on('scroll', function () {return onScroll(contentHeaderHeight);});
 
-  function onScroll(contentHeaderHeight) {
-    let scrollPos = $(document).scrollTop();
-    // if (scrollPos > headerOffset.top) {
-    //  // elementHeader.css('position', 'fixed').css('width', '100%');
-    //  // elementContentHeader.css('height', contentHeaderHeight);
-    // } else {
-    //  // elementHeader.css('position', 'relative');
-    //   //elementContentHeader.css('height', '33px');
-    // }
-    $('ul#menu li a').each(function(){return defineMenuPosition($(this), scrollPos + contentHeaderHeight);});
-
-     //smoothscroll
-     $('ul#menu li a').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");
-      
-      $('a').each(function () {
-        $(this).parent().css('background', 'red');
-      })
-      $(this).parent().addClass('selected');
-    
-      let target = this.hash,
-          menu = target;
-      let $anchor = $(target);
-      $('html, body').stop().animate({
-          'scrollTop': $anchor.offset().top+contentHeaderHeight
-      }, 300, 'swing', function () {
-          window.location.hash = target;
-          $(window).on("scroll", function(){return onScroll(contentHeaderHeight);});
-      });
-  });
-  }
-
-  function defineMenuPosition($t, changePosition) {
-    var currLink = $t;
-    var refElement = $(currLink.attr("href"));
-    if (refElement.position().top <= changePosition) {
-      $('ul#menu li').removeClass("selected");
-      currLink.parent().addClass("selected");
-    }
-    else {
-      currLink.removeClass("selected");
-    }
-  }
+  $('ul#menu li a').on('click', function () {return goToSection(this.hash, event) });
 });
+
+function onScroll(contentHeaderHeight) {
+  let scrollPos = $(document).scrollTop();
+  // if (scrollPos > headerOffset.top) {
+  //  // elementHeader.css('position', 'fixed').css('width', '100%');
+  //  // elementContentHeader.css('height', contentHeaderHeight);
+  // } else {
+  //  // elementHeader.css('position', 'relative');
+  //   //elementContentHeader.css('height', '33px');
+  // }
+  $('ul#menu li a').each(function () {
+    return defineMenuPosition($(this), scrollPos + contentHeaderHeight);
+  });
+}
+
+//smoothscroll
+function goToSection(target, e) {
+  e.preventDefault();
+  console.log(e);
+  $(document).off("scroll");
+  // let target = this.hash;
+  let menu = target;
+  let $anchor = $(target);
+  $('html, body').stop().animate({ 'scrollTop': $anchor.offset().top - 210 }, 500, 'swing', function () {
+    $(window).on("scroll", function () { return onScroll(210); });
+  });
+}
+
+function defineMenuPosition($this, changePosition) {
+  var refElement = $($this.attr("href"));
+  if (changePosition < refElement.position().top && refElement.attr('id') === 'section_1') {
+    $this.parent().addClass("selected");
+  }
+  else if (refElement.position().top <= changePosition && refElement.position().top + refElement.height() > changePosition) {
+    $('ul#menu li').removeClass("selected");
+    $this.parent().addClass("selected");
+  }
+  else {
+    $this.parent().removeClass("selected");
+  }
+}
+
+function locationHashChanged() {
+  console.log('lalalalas');
+}
